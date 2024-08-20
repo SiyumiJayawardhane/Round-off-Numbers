961a52ac9f2caa25fefd55da18bfc370db4ee845
Siyumi  Jayawardhane <139908557+SiyumiJayawardhane@users.noreply.github.com>
2024-08-20 19:52:11 +0530

    Add files via upload

---
+++ roundOffnumbers.java
@@ -0,0 +1,9 @@
+package JavaWeek2Tut;
+
+public class roundOffnumbers {
+    public static void main(String[] args) {
+        double v1=9.888867d;
+        System.out.printf("The value rounded to two decimal points is: %.3f%n" ,v1);
+    }
+}
+
